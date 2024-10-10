82ce2af587bc653f767a3d9cc690a1795c73d106
santiago <santiagoneira52@gmail.com>
2024-10-10 13:35:12 -0300

    Inicio proyecto, conexión a contenedor con mySQL

---
+++ src/main/java/com/santi/gestor_tareas_ACL/GestorTareasAclApplication.java
@@ -0,0 +1,13 @@
+package com.santi.gestor_tareas_ACL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorTareasAclApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorTareasAclApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santi/gestor_tareas_ACL/GestorTareasAclApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santi.gestor_tareas_ACL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorTareasAclApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
