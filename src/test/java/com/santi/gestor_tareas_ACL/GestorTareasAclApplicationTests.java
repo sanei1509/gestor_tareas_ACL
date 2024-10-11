0d2721923aa8641d10d4871b600de2ee11693102
santiago <santiagoneira52@gmail.com>
2024-10-11 10:43:58 -0300

    Test unitario de conexión a la base de datos

--- src/test/java/com/santi/gestor_tareas_ACL/GestorTareasAclApplicationTests.java
+++ src/test/java/com/santi/gestor_tareas_ACL/GestorTareasAclApplicationTests.java
@@ -1,14 +1,21 @@
 package com.santi.gestor_tareas_ACL;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-class GestorTareasAclApplicationTests {
+public class GestorTareasAclApplicationTests {
+
+	@Autowired
+	private DataSource dataSource;
 
 	@Test
-	void contextLoads() {
+	public void testDatabaseConnection() throws Exception {
+		assertNotNull(dataSource.getConnection(), "La conexión a la base de datos no puede ser nula");
 	}
-	
-
 }
